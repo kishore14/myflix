46fb3fb8167c4b0832b308805fd735edcd3715ba
Kishore <kishore.rambatla@gmail.com>
2015-06-04 20:57:01 +0000

    pre-course

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,14 @@ Video.create!(title: 'Family Guy',
               description: 'In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.',
               small_cover_url: '/tmp/family_guy.jpg',
               category: comedies)
+Video.create!(title: 'Futurama',
+              description: 'Fry, a pizza guy is accidentally frozen in 1999 and thawed out New Year\'s Eve 2999.',
+              small_cover_url: '/tmp/futurama.jpg',
+              category: comedies)
+
+
+dramas = Category.create!(name: "TV Drama")              
+Video.create!(title: 'South Park',
+              description: 'Follows the misadventures of four irreverent grade-schoolers in the quiet, dysfunctional town of South Park, Colorado.',
+              small_cover_url: '/tmp/south_park.jpg',
+              category: dramas)
