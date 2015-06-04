f1d4d11e4006311db41b7abf97b3616d6ec59cf4
Kishore <kishore.rambatla@gmail.com>
2015-06-04 18:32:51 +0000

    pre-course

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
+  
+  get '/home', to: "videos#index"
+  
+  root to: "videos#index"
+  resources :videos
+  resources :categories
 end
